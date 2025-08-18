aa7defbdbaff5ebd20db2e8b8fe0fe12f772811f
mevro <mevro@students.cs.ubc.ca>
2025-08-17 23:22:51 -0600

    chore: ignore node_modules and env/build artifacts, updated README and documentation

--- faceforward-platform/src/index.js
+++ faceforward-platform/src/index.js
@@ -5,10 +5,10 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
  
 const root = createRoot(document.getElementById('root')); 
+// 
 
 root.render(
     <App />
-
 );
 
 // If you want to start measuring performance in your app, pass a function
