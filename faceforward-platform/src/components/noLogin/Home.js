af2c979fda8cb6efa0b972b21cc34147b40e37f2
Mark Evro <mackoshkamacka@gmail.com>
2025-09-01 23:59:45 -0700

    fixed bugs with hero

--- faceforward-platform/src/components/noLogin/Home.js
+++ faceforward-platform/src/components/noLogin/Home.js
@@ -4,16 +4,24 @@ export default function Home() {
   return (
     <div className="homeContainer">
         <div className="hero">
-            <div className = "heroLeft">
+            <div className = "heroBottom">
                 {/* slash goes here  */}
             </div>
-            <div className = "heroRight">
-                <h1 className="title">FACE FORWARD</h1> 
-                <div className="heroRightText">                    
-                    <h3 className = "missionStatement">...Mission Statement...</h3>
-                    <p>...Supporting Tagline...</p>
+            <div className = "heroTop">
+                <div className = "title">
+                    <h1 className="title1">FACE</h1> 
+                    <h1 className="title2">FORWARD</h1>
+                    <h2 className="subtitle">Humanitarian Association </h2>
+                </div>
+                <div className="heroDesc">                    
+                    <h3 className = "missionStatement">Mission Statement: </h3>
+                    <p className = "missionStatementP">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                        nisi ut aliquip ex ea commodo consequat. "</p>
                 </div>
             </div>
+            
         </div>
         <div className="impact">
             <p>...stats...hospitals associated (if too large make it # of), # of patients helped, # of artists etc.</p>    
